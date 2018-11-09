eda69d272591120ae8eb6d3911ac6fd14dda42e0
Екатерина Бакалдина <e.bakaldina@city-mobil.ru>
2018-11-09 14:47:28 +0300

    init

---
+++ script.js
@@ -0,0 +1,463 @@
+var data = {
+    "city_operators": {
+        "members": {
+            "4201": {
+                "uid": "4201",
+                "status": "pickup",
+                "ringStatusHour": "0\/5\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4203": {
+                "uid": "4203",
+                "status": "dialout",
+                "ringStatusHour": "0\/26\/0",
+                "ringStatusCurrent": "0\/13\/0"
+            },
+            "4205": {
+                "uid": "4205",
+                "status": "pickup",
+                "ringStatusHour": "0\/7\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4206": {
+                "uid": "4206",
+                "status": "pickup",
+                "ringStatusHour": "0\/10\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4208": {
+                "uid": "4208",
+                "status": "pickup",
+                "ringStatusHour": "0\/11\/0",
+                "ringStatusCurrent": "0\/1\/0"
+            },
+            "4214": {
+                "uid": "4214",
+                "status": "block",
+                "ringStatusHour": "0\/3\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4215": {
+                "uid": "4215",
+                "status": "block",
+                "ringStatusHour": "0\/1\/4",
+                "ringStatusCurrent": "0\/0\/6"
+            },
+            "4216": {
+                "uid": "4216",
+                "status": "pickup",
+                "ringStatusHour": "0\/1\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4220": {
+                "uid": "4220",
+                "status": "pickup",
+                "ringStatusHour": "0\/0\/1",
+                "ringStatusCurrent": "0\/1\/1"
+            },
+            "4227": {
+                "uid": "4227",
+                "status": "pickup",
+                "ringStatusHour": "0\/0\/1",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4231": {
+                "uid": "4231",
+                "status": "block",
+                "ringStatusHour": "0\/4\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4232": {
+                "uid": "4232",
+                "status": "block",
+                "ringStatusHour": "0\/1\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4240": {
+                "uid": "4240",
+                "status": "pickup",
+                "ringStatusHour": "0\/14\/0",
+                "ringStatusCurrent": "0\/0\/1"
+            },
+            "4241": {
+                "uid": "4241",
+                "status": "pickup",
+                "ringStatusHour": "0\/5\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4242": {
+                "uid": "4242",
+                "status": "block",
+                "ringStatusHour": "0\/36\/0",
+                "ringStatusCurrent": "0\/1\/1"
+            },
+            "4245": {
+                "uid": "4245",
+                "status": null,
+                "ringStatusHour": "0\/1\/0",
+                "ringStatusCurrent": "0\/3\/0"
+            },
+            "4247": {
+                "uid": "4247",
+                "status": "pickup",
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4248": {
+                "uid": "4248",
+                "status": "block",
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5208": {
+                "uid": "5208",
+                "status": "pickup",
+                "ringStatusHour": "0\/1\/0",
+                "ringStatusCurrent": "0\/4\/0"
+            },
+            "5233": {
+                "uid": "5233",
+                "status": "block",
+                "ringStatusHour": "0\/4\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5237": {
+                "uid": "5237",
+                "status": "pickup",
+                "ringStatusHour": "0\/4\/0",
+"ringStatusCurrent": "0\/2\/0"
+            },
+            "5238": {
+                "uid": "5238",
+                "status": "block",
+                "ringStatusHour": "0\/3\/1",
+                "ringStatusCurrent": "0\/1\/0"
+            }
+        },
+        "stats": {
+            "clientsInQueue": "2",
+            "freeOperators": 0,
+            "avgWait5": 23,
+            "avgWait30": 4,
+            "avgWait60": 3,
+            "callsAnswered5": "54",
+            "callsAnswered30": "209",
+            "callsAnswered60": "409",
+            "callsNotAnswered5": "14",
+            "callsNotAnswered30": "29",
+            "callsNotAnswered60": "29"
+        }
+    },
+    "city_managers": {
+        "members": {
+            "4201": {
+                "uid": "4201",
+                "status": "pickup",
+                "ringStatusHour": "0\/5\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4203": {
+                "uid": "4203",
+                "status": "dialout",
+                "ringStatusHour": "0\/26\/0",
+                "ringStatusCurrent": "0\/13\/1"
+            },
+            "4205": {
+                "uid": "4205",
+                "status": "pickup",
+                "ringStatusHour": "0\/7\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4206": {
+                "uid": "4206",
+                "status": "pickup",
+                "ringStatusHour": "2\/10\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4208": {
+                "uid": "4208",
+                "status": "pickup",
+                "ringStatusHour": "2\/11\/0",
+                "ringStatusCurrent": "0\/1\/1"
+            },
+            "4211": {
+                "uid": "4211",
+                "status": null,
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4214": {
+                "uid": "4214",
+                "status": "block",
+                "ringStatusHour": "0\/3\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4231": {
+                "uid": "4231",
+                "status": "block",
+                "ringStatusHour": "5\/4\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4232": {
+                "uid": "4232",
+                "status": "block",
+                "ringStatusHour": "0\/1\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4239": {
+                "uid": "4239",
+                "status": "pickup",
+                "ringStatusHour": "0\/8\/1",
+                "ringStatusCurrent": "0\/2\/0"
+            },
+            "4240": {
+                "uid": "4240",
+                "status": "pickup",
+                "ringStatusHour": "2\/14\/1",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4241": {
+                "uid": "4241",
+                "status": "pickup",
+                "ringStatusHour": "0\/5\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "4242": {
+                "uid": "4242",
+                "status": "block",
+                "ringStatusHour": "2\/36\/0",
+                "ringStatusCurrent": "0\/1\/0"
+            },
+            "4248": {
+                "uid": "4248",
+                "status": "block",
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5000": {
+                "uid": "5000",
+                "status": null,
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5204": {
+                "uid": "5204",
+                "status": null,
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5205": {
+                "uid": "5205",
+                "status": "pickup",
+                "ringStatusHour": "3\/8\/0",
+                "ringStatusCurrent": "0\/2\/0"
+            },
+            "5208": {
+                "uid":
+"5208",
+                "status": "pickup",
+                "ringStatusHour": "1\/1\/0",
+                "ringStatusCurrent": "1\/4\/0"
+            },
+            "5209": {
+                "uid": "5209",
+                "status": "block",
+                "ringStatusHour": "0\/41\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5211": {
+                "uid": "5211",
+                "status": "pickup",
+                "ringStatusHour": "1\/1\/0",
+                "ringStatusCurrent": "1\/4\/0"
+            },
+            "5213": {
+                "uid": "5213",
+                "status": null,
+                "ringStatusHour": "0\/2\/0",
+                "ringStatusCurrent": "0\/1\/0"
+            },
+            "5217": {
+                "uid": "5217",
+                "status": "block",
+                "ringStatusHour": "1\/7\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5218": {
+                "uid": "5218",
+                "status": null,
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5220": {
+                "uid": "5220",
+                "status": null,
+                "ringStatusHour": "0\/2\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5221": {
+                "uid": "5221",
+                "status": "pickup",
+                "ringStatusHour": "0\/3\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5223": {
+                "uid": "5223",
+                "status": null,
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/1\/0"
+            },
+            "5227": {
+                "uid": "5227",
+                "status": "block",
+                "ringStatusHour": "1\/5\/0",
+                "ringStatusCurrent": "0\/1\/0"
+            },
+            "5232": {
+                "uid": "5232",
+                "status": "block",
+                "ringStatusHour": "0\/2\/0",
+                "ringStatusCurrent": "0\/1\/0"
+            },
+            "5233": {
+                "uid": "5233",
+                "status": "block",
+                "ringStatusHour": "2\/4\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5237": {
+                "uid": "5237",
+                "status": "pickup",
+                "ringStatusHour": "0\/4\/0",
+                "ringStatusCurrent": "1\/2\/0"
+            },
+            "5238": {
+                "uid": "5238",
+                "status": "block",
+                "ringStatusHour": "2\/3\/0",
+                "ringStatusCurrent": "0\/1\/0"
+            },
+            "5239": {
+                "uid": "5239",
+                "status": "block",
+                "ringStatusHour": "2\/3\/0",
+                "ringStatusCurrent": "0\/4\/7"
+            },
+            "5240": {
+                "uid": "5240",
+                "status": "block",
+                "ringStatusHour": "2\/9\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5241": {
+                "uid": "5241",
+                "status": "block",
+                "ringStatusHour": "2\/42\/0",
+                "ringStatusCurrent": "0\/16\/0"
+            },
+            "5242": {
+                "uid": "5242",
+                "status": null,
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5243": {
+                "uid": "5243",
+                "status": null,
+                "ringStatusHour": "0\/0\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5246": {
+                "uid": "5246",
+                "status": "block",
+                "ringStatusHour": "2\/11\/0",
+                "ringStatusCurrent": "0\/4\/0"
+            },
+            "5249": {
+                "uid": "5249",
+                "status": "pickup",
+                "ringStatusHour": "2\/10\/0",
+                "ringStatusCurrent": "0\/3\/0"
+            },
+            "5250": {
+                "uid": "5250",
+"status": "block",
+                "ringStatusHour": "1\/3\/0",
+                "ringStatusCurrent": "0\/3\/0"
+            },
+            "5252": {
+                "uid": "5252",
+                "status": "block",
+                "ringStatusHour": "1\/5\/0",
+                "ringStatusCurrent": "0\/0\/0"
+            },
+            "5602": {
+                "uid": "5602",
+                "status": "dialout",
+                "ringStatusHour": "3\/2\/0",
+                "ringStatusCurrent": "0\/4\/0"
+            }
+        },
+        "stats": {
+            "clientsInQueue": "1",
+            "freeOperators": 0,
+            "avgWait5": 67,
+            "avgWait30": 4,
+            "avgWait60": 4,
+            "callsAnswered5": "2",
+            "callsAnswered30": "17",
+            "callsAnswered60": "41",
+            "callsNotAnswered5": "1",
+            "callsNotAnswered30": "2",
+            "callsNotAnswered60": "2"
+        }
+    }
+};
+
+function loadData() {
+    // $.get(
+    //     "operator_status_data.json",
+    //     {
+    //         param1: "param1",
+    //         param2: 2
+    //     },
+    //     function (data) {
+    //         console.log(data);
+    //         console.log(data.city_operators.stats.clientsInQueue);
+
+
+            let types = ['operators', 'managers'];
+            let fields = ['clientsInQueue', 'freeOperators', 'avgWait5', 'avgWait30', 'avgWait60', 'callsAnswered5','callsAnswered30','callsAnswered60', 'callsNotAnswered5', 'callsNotAnswered30','callsNotAnswered60' ];
+
+
+            types.forEach(function(type) {
+                Object.keys(data['city_' + type]['members']).forEach(function(key) {
+                    let member = data['city_' + type]['members'][key];
+                    let element = $('#cell' + member.uid);
+
+                    if (element.length) {
+                        element[0].classList.forEach(function(className) {
+                            if (className.value != 'block') element.removeClass(className.value);
+                        });
+
+                        if (member.status) {
+                            element.addClass(member.status);
+                        }
+                    }
+                });
+            });
+
+            types.forEach(function (type) {
+                fields.forEach(function (field) {
+                    $('#' + type + field.charAt(0).toUpperCase() + field.slice(1))[0].innerHTML = data['city_' + type].stats[field];
+                });
+            });
+        };
+//     );
+// }
+
+$(function() {
+   loadData();
+//    setInterval(loadData, 5000);
+});
+
